bde40bc0cdd2dff6049775ab97f643a49c552c53
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-06-24 11:14:39 +0530

    add 3 number in java

---
+++ Add3Num.java
@@ -0,0 +1,10 @@
+public class Add3Num{
+    public static void main(String[] args){
+        int num1 = 4 ;
+        int num2 = 8 ;
+        int num3 = 8 ;
+        int sum = num1 + num2 + num3 ;
+        System.out.println("the sum is : " + sum);
+    }
+}
+  
