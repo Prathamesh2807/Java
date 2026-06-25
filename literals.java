9663089dacb0eb52eab4de597469dba506558204
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-06-25 10:55:29 +0530

    literals are the constant assign to variables

---
+++ literals.java
@@ -0,0 +1,10 @@
+public class literals {
+    public static void main(String[] args) {
+        int age = 34 ;
+        //char ch = 'a' ;
+        System.out.println(age);
+        float f1 = 10.1f ;
+        double d1 = 10.2d ;
+        System.out.println(f1+d1);
+    }
+}
