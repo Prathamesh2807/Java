d81e1224295e57ff4f6b3c949c5a591d26364a78
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-06-29 16:16:22 +0530

    used new str

--- src/com/prathamesh/Convert_lower.java
+++ src/com/prathamesh/Convert_lower.java
@@ -1,6 +1,13 @@
 package com.prathamesh;
-
+import java.util.Scanner ;
 public class Convert_lower {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter th name : ");
+        String name = sc.next();
+        String name_new = name.toLowerCase();
+        System.out.println(name_new);
+        System.out.println(name.toLowerCase());
+    }
 
 }
