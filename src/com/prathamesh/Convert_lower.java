6a0ce8a75d7d5be3bd5d26cc84581e358a14c25c
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-06-29 14:46:06 +0530

    string as input

---
+++ src/com/prathamesh/Convert_lower.java
@@ -0,0 +1,6 @@
+package com.prathamesh;
+
+public class Convert_lower {
+    public static void main(String[] args){}
+
+}
---
+++ src/com/prathamesh/Cwh_13_strings.java
@@ -0,0 +1,13 @@
+package com.prathamesh;
+
+import java.util.Scanner;
+public class Cwh_13_strings{
+      public static void main(String[] args){
+         //String name = "prathamesh";
+         //  System.out.println(name);
+         Scanner sc = new Scanner(System.in);
+         System.out.println("enter the name : ");
+         String name = sc.nextLine();
+         System.out.printf("your name is : %s" , name);
+      }
+}
