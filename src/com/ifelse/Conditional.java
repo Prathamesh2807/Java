1b8c68e934450b62c85db096e9e9c49240a8d787
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-06-30 20:30:47 +0530

    if else statement in java

---
+++ src/com/ifelse/Conditional.java
@@ -0,0 +1,13 @@
+package com.ifelse;
+
+public class Conditional {
+    public static void main(String[] args){
+        int age = 20;
+        if (age > 18) {
+            System.out.println("you can drive bro");
+        }
+        else {
+            System.out.println("grow up buddy ");
+        }
+    }
+}
