89c8e95342c26b35a8232fa319c1712fd5708b48
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-07-02 20:10:09 +0530

    else if statement in javva

---
+++ src/com/ifelse/Else_if.java
@@ -0,0 +1,17 @@
+package com.ifelse;
+import java.util.Scanner;
+public class Else_if {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your age : ");
+        int age = sc.nextInt();
+
+        if (age>18){
+            System.out.println("you can drive");
+        }
+        else if (age == 18){
+            System.out.println("do you have card ");
+        }
+        else System.out.println("grow up child");
+    }
+}
